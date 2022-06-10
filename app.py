b1b7cb27a2253d3135e993881a09c10758d01325
BhavyaBibi <bhavyabibi007@gmail.com>
2022-06-10 11:15:30 +0400

    schedule is added for minutes

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 import schedule
 import time
 
+def job():
+    print("im working")
+def coding():
+    print("im coding")
+
+schedule.every(5).minutes.do(job) 
+schedule.every(15).minutes.do(coding)  
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
